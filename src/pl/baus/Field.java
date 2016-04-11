35cccb3ced86d333e7dfe5d6d682fb9282b64702
dibart227 <dibart227@gmail.com>
2016-04-11 23:51:56 +0200

    Podzielenie kodu na obiekty do których należą.

---
+++ src/pl/baus/Field.java
@@ -0,0 +1,38 @@
+package pl.baus;
+
+/**
+ * Created by Bartek on 2016-04-11.
+ */
+public class Field {
+
+    private final int x;
+    private final int y;
+    private State state;
+
+    public Field(int x, int y, State state){
+        this.x = x;
+        this.y = y;
+        this.state = state;
+    }
+
+    public char stateToChar() {
+        char value;
+        switch (state){
+            case EMPTY:
+                value = ' ';
+                break;
+            case HIT:
+                value = 'O';
+                break;
+            default:
+                value = '?';
+                break;
+        }
+        return value;
+    }
+
+    public void setState(State state){
+        this.state = state;
+    }
+
+}
---
+++ src/pl/baus/State.java
@@ -0,0 +1,8 @@
+package pl.baus;
+
+/**
+ * Created by Bartek on 2016-04-11.
+ */
+enum State {
+    EMPTY, SUNK, HIT, MISS
+}
